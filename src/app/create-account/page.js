f54330855132086f927780e46de8827215331446
Zohar Williams <zoharkiks@gmail.com>
2023-11-04 12:59:16 +0530

    Init Login Page

--- src/app/create-account/page.js
+++ src/app/create-account/page.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../components/Button";
 
 const CreateAccount = () => {
   return (
-    <div>CreateAccount</div>
-  )
-}
+    <div className="padding">
+      <div className="flex">
+        <span class="font-bold">CheckOff</span>
+      </div>
+      <div className="grid place-items-center ">
+        <h1 className="">You are in for a ride </h1>
 
-export default CreateAccount
+        <form className="flex" action="">
+          <input
+            className="border"
+            placeholder="name@company.com"
+            type="email"
+            name=""
+            id=""
+            required
+          />
+          <Button>SignUp</Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateAccount;
