48c188b4e57ce8b4aee621dcce99f9b6dbef0bf4
Zohar Williams <zoharkiks@gmail.com>
2023-11-04 17:49:35 +0530

    Add user schema

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,11 +1,18 @@
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from "uuid";
+import { connectMongoDB } from "../../../../lib/mongodb";
+import User from "../../../../models/user";
+import bcrypt from "bcryptjs";
+
 
 export const POST = async (req) => {
   try {
     const {email, password} = await req.json()
+    const hashedPassword = await bcrypt.hash(password,10)
 
-    console.log(email,password);
+    await connectMongoDB()
+    await User.create({email, password:hashedPassword})
+
 
   return NextResponse.json({message:'User Registered'},{status:'200'})
   } catch (error) {
